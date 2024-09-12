a7c0df9e4ecfb6ac8527df04d0e77ab32997b6b0
lyflexi <liuyanoutsee@outlook.com>
2024-09-12 22:56:19 +0800

    [a]@RestControllerAdvice最佳实践

---
+++ debug_springboot/src/main/java/org/lyflexi/debug_springboot/exception/controller/MockController.java
@@ -0,0 +1,30 @@
+package org.lyflexi.debug_springboot.exception.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.lyflexi.debug_springboot.exception.LyFlexiBusinessException;
+import org.lyflexi.debug_springboot.exception.LyflexiErrorType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: lyflexi
+ * @project: debuginfo_jdkToFramework
+ * @Date: 2024/8/23 8:21
+ */
+@RestController
+@RequestMapping("/mock")
+@Slf4j
+public class MockController {
+
+    @GetMapping(value = "/formatException")
+    public void formatExceptionAdvice (){
+        throw new LyFlexiBusinessException(LyflexiErrorType.NO_PULL_REPLENISH_CONFIG,new Object[]{"materialCode", "materialName","materialVersion"});
+    }
+
+    @GetMapping(value = "/formatExceptionAhead")
+    public void formatException (){
+        throw LyFlexiBusinessException.exception("需求[{0}]关联的工单排程基础信息丢失，无法继续执行！", "demandCode");
+    }
+}
