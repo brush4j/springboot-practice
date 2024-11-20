b07b51c68c88392fa35769704d285704dd0ba48b
lyflexi <liuyanoutsee@outlook.com>
2024-11-20 19:58:02 +0800

    pure

---
+++ springboot-pure/src/main/java/com/lyflexi/springbootpure/SpringbootPureApplication.java
@@ -0,0 +1,13 @@
+package com.lyflexi.springbootpure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPureApplication.class, args);
+    }
+
+}
